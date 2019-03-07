faece0fd08432872e2d049f2f32a1122d2df3bff
Joshua <joshuapaulsmyth@gmail.com>
2019-03-07 19:56:39 +1300

    Quick hack implementation of a wait node.

--- DialogML/RNodes/RNodeWait.cs
+++ DialogML/RNodes/RNodeWait.cs
@@ -1,6 +1,7 @@
 ﻿using DialogML.DNodes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +17,37 @@ namespace DialogML.RNodes
 
     public class RNodeWait : RNode
     {
+        WaitModeState state;    // TODO Shift state byte to base class and access via runtime table
+
         Int32 Milliseconds;
+        Stopwatch sw = new Stopwatch(); // TODO Grab from some pool
         public RNodeWait(Guid id, Int32 milliseconds)
         {
             Id = id;
             Milliseconds = milliseconds;
+            state = WaitModeState.NotStarted;
         }
 
         public override AdvanceType Execute(ScriptApi api)
         {
-            api.Trace("RNode: Wait");
+            if (state == WaitModeState.NotStarted)
+            {
+                sw.Start();
+                state = WaitModeState.Started;
+                return AdvanceType.Yield;
+            }
+            if (state == WaitModeState.Started)
+            {
+                if (sw.ElapsedMilliseconds >= Milliseconds)
+                {
+                    sw.Stop();
+                    state = WaitModeState.Finished;
+                    return AdvanceType.Next;
+                }
+                return AdvanceType.Yield;
+            }
+
+            //api.Trace("RNode: Wait");
             return AdvanceType.Next;
         }
 
