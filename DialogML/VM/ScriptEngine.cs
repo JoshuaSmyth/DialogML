789d5ca3ae946bb6020e14f542de474a66590f24
Joshua <joshuapaulsmyth@gmail.com>
2019-03-11 21:16:18 +1300

    Minor Refactor of script engine running loop.

--- DialogML/VM/ScriptEngine.cs
+++ DialogML/VM/ScriptEngine.cs
@@ -75,15 +75,16 @@ namespace DialogML
     public class ScriptEngine
     {
         public ScriptEngineStatus Status;
-
-        //public AdvanceType RV;
+        
         public Guid JumpRegister;
         public Int32 ChildNRegister;
 
+        // TODO Extract into ProgramTable
         public OnlyIfTable m_OnlyIfTable = new OnlyIfTable();
         public StringTable m_StringTable = new StringTable();
         public RuntimeReferencesTable m_ReferencesTable = new RuntimeReferencesTable();
 
+        // TODO Extract into execution units
         public Stack<RNode> m_ProgramStack = new Stack<RNode>();    // This is how we navigate the tree
         public Stack<int> m_IndexStack = new Stack<int>();          // Keep a record of what child index we are at.
         public Stack<int> m_ReturnStack = new Stack<int>();         // index into the program stack to change the default behaviour
@@ -93,7 +94,6 @@ namespace DialogML
         
         public ScriptEngine(RuntimeReferencesTable referencesTable, StringTable stringTable, OnlyIfTable onlyIfTable)
         {
-            // TODO Add String Table
             m_StringTable = stringTable;
             m_OnlyIfTable = onlyIfTable;
             m_ReferencesTable = referencesTable;
@@ -143,7 +143,6 @@ namespace DialogML
         internal void PushReturnParentNode()
         {
             throw new Exception("Don't call this method TODO Remove");
-            m_ReturnStack.Push(m_ProgramStack.Count-1);
         }
 
         internal void PushReturnCurrentNode()
@@ -161,125 +160,122 @@ namespace DialogML
 
             var currentNode = m_ProgramStack.Peek();
             var rv = advanceType;
-           // AdvanceType rv = currentNode.Execute(m_ScriptApi);
 
             while(rv != AdvanceType.Yield &&
                   rv != AdvanceType.Finished)
             {
-                if (rv == AdvanceType.Return)
+                switch(rv)
                 {
-                    if(m_ProgramStack.Count > 0)
-                    {
-                        currentNode = m_ProgramStack.Pop();
-                        m_IndexStack.Pop();
-                    }
-                    else
-                    {
-                        currentNode = null;
-                    }
-                }
-                if (rv == AdvanceType.JumpToNode)
-                {
-                    // Push Call Node
-                    m_ProgramStack.Push(currentNode);
-                    m_IndexStack.Push(currentNode.Children.Count); // Should be 0
-
-                    // TODO Implement Get by Id
-                    // TODO come up with something better than the 'as' statement 
-                    
-                    var name = currentNode as RNodeCallPage;
-                    currentNode =  m_ReferencesTable.GetPageByName(name.PageName);
-                    
-                    // Push the destination Node
-                    m_ProgramStack.Push(currentNode);
-                    m_IndexStack.Push(0);
-                }
-                
-                if (rv == AdvanceType.ChildN)
-                {
-                    int pushValue = 0;
-                    currentNode = currentNode.Children[ChildNRegister];
-                    m_ProgramStack.Push(currentNode);
-                    m_IndexStack.Push(pushValue);
-                }
-
-                if(rv == AdvanceType.FirstChild)
-                {
-                    int pushValue = 0;
-                    
-                    currentNode = currentNode.Children[0];
-                    m_ProgramStack.Push(currentNode);
-                    m_IndexStack.Push(pushValue);
-                }
-
-                if(rv == AdvanceType.SecondChild)
-                {
-                    int pushValue = 0;
-
-                    currentNode = currentNode.Children[1];
-                    m_ProgramStack.Push(currentNode);
-                    m_IndexStack.Push(pushValue);
-                }
-
-                if(rv == AdvanceType.Next)
-                {
-                    // Choose next sibling.
-                    // If no siblings left, choose parents sibling.
-
-                    currentNode = m_ProgramStack.Pop();
-                    var index = m_IndexStack.Pop();
-                    
-                    if(m_ProgramStack.Count == 0)
-                    {
-                        rv = AdvanceType.Finished;
-                        continue;
-                    }
-
-                    var parent = m_ProgramStack.Peek();
-                   
-                    index++;
-                    if(index >= parent.Children.Count)
-                    {
-                        // Check the return stack if we need to enter a particular
-                        // location.
-                        if (m_ReturnStack.Count > 0)
+                    case AdvanceType.Return:
                         {
-                            var pindex = m_ReturnStack.Pop();
-                            var indexValue = 0;
-                            while(m_ProgramStack.Count > pindex)
+                            if(m_ProgramStack.Count > 0)
                             {
-                                m_ProgramStack.Pop();
-                                indexValue = m_IndexStack.Pop();
+                                currentNode = m_ProgramStack.Pop();
+                                m_IndexStack.Pop();
                             }
-                            currentNode = m_ProgramStack.Peek();
-                            
-                            rv = AdvanceType.Continue;
-                            continue;
+                            else
+                            {
+                                currentNode = null;
+                            }
+                            break;
                         }
+                    case AdvanceType.JumpToNode:
+                        {
+                            // Push Call Node
+                            m_ProgramStack.Push(currentNode);
+                            m_IndexStack.Push(currentNode.Children.Count); // Should be 0
 
+                            // TODO Implement Get by Id
+                            // TODO come up with something better than the 'as' statement 
+                            var name = currentNode as RNodeCallPage;
+                            currentNode = m_ReferencesTable.GetPageByName(name.PageName);
 
-                        rv = AdvanceType.Next;
-                        continue;
-                    }
-                    else
-                    {
-                        currentNode = parent.Children[index];
-                        m_ProgramStack.Push(currentNode);
-                        m_IndexStack.Push(index);
-                    }
-                }
+                            // Push the destination Node
+                            m_ProgramStack.Push(currentNode);
+                            m_IndexStack.Push(0);
+                            break;
+                        }
+                    case AdvanceType.ChildN:
+                        {
+                            int pushValue = 0;
+                            currentNode = currentNode.Children[ChildNRegister];
+                            m_ProgramStack.Push(currentNode);
+                            m_IndexStack.Push(pushValue);
+                            break;
+                        }
+                    case AdvanceType.FirstChild:
+                        {
+                            int pushValue = 0;
+                            currentNode = currentNode.Children[0];
+                            m_ProgramStack.Push(currentNode);
+                            m_IndexStack.Push(pushValue);
+                            break;
+                        }
+                    case AdvanceType.SecondChild:
+                        {
+                            int pushValue = 0;
+                            currentNode = currentNode.Children[1];
+                            m_ProgramStack.Push(currentNode);
+                            m_IndexStack.Push(pushValue);
+                            break;
+                        }
+                    case AdvanceType.Next:
+                        {
+                            // Choose next sibling.
+                            // If no siblings left, choose parents sibling.
+                            currentNode = m_ProgramStack.Pop();
+                            var index = m_IndexStack.Pop();
 
-                if (rv == AdvanceType.Parent)
-                {
-                    if(m_ProgramStack.Count > 0)
-                    {
-                        currentNode = m_ProgramStack.Pop();
-                        m_IndexStack.Pop();
-                    }
-                    else
-                    {
-                        currentNode = null;
-                    }
+                            if(m_ProgramStack.Count == 0)
+                            {
+                                rv = AdvanceType.Finished;
+                                continue;
+                            }
+
+                            var parent = m_ProgramStack.Peek();
+                            index++;
+                            if(index >= parent.Children.Count)
+                            {
+                                // Check the return stack if we need to enter a particular location.
+                                if(m_ReturnStack.Count > 0)
+                                {
+                                    var pindex = m_ReturnStack.Pop();
+                                    var indexValue = 0;
+                                    while(m_ProgramStack.Count > pindex)
+                                    {
+                                        m_ProgramStack.Pop();
+                                        indexValue = m_IndexStack.Pop();
+                                    }
+                                    currentNode = m_ProgramStack.Peek();
+
+                                    rv = AdvanceType.Continue;
+                                    continue;
+                                }
+
+                                rv = AdvanceType.Next;
+                                continue;
+                            }
+                            else
+                            {
+                                currentNode = parent.Children[index];
+                                m_ProgramStack.Push(currentNode);
+                                m_IndexStack.Push(index);
+                            }
+                            break;
+                        }
+                    case AdvanceType.Parent:
+                        {
+                            if(m_ProgramStack.Count > 0)
+                            {
+                                currentNode = m_ProgramStack.Pop();
+                                m_IndexStack.Pop();
+                            }
+                            else
+                            {
+                                currentNode = null;
+                            }
+                            break;
+                        }
                 }
 
                 if(currentNode == null)
